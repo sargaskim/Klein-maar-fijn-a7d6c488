11d31cfd98da7f982b8815cdd08218cef4be2156
sargaskim <2103702@talnet.nl>
2020-02-24 11:28:38 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,60 @@
+<?PHP
+
+$totaal = (float)$argv[1];
+
+if ($totaal != 0){
+
+    $wisselgeld = floor($totaal/10);
+    if ($wisselgeld != 0){
+    echo $wisselgeld. " x €10 billetten".PHP_EOL;
+    $totaal -= $wisselgeld * 10;
+    }
+
+    $wisselgeld = floor($totaal/5);
+    if ($wisselgeld != 0){
+    echo $wisselgeld. " x €5 billetten".PHP_EOL;
+    $totaal -= $wisselgeld * 5;
+    }
+
+    $wisselgeld = floor($totaal/2);
+    if ($wisselgeld != 0){
+    echo $wisselgeld. " x €2 munten".PHP_EOL;
+    $totaal -= $wisselgeld * 2;
+    }
+    
+    $wisselgeld = floor($totaal/1);
+    if ($wisselgeld != 0){
+    echo $wisselgeld. " x €1 munten".PHP_EOL;
+    $totaal -= $wisselgeld * 1;        
+    }
+
+    $wisselgeld = floor($totaal/0.5);
+    if ($wisselgeld != 0){
+    echo $wisselgeld. " x 50c munten".PHP_EOL;
+    $totaal -= $wisselgeld * 0.5;        
+    }
+
+    $wisselgeld = floor($totaal/0.2);
+    if ($wisselgeld != 0){
+    echo $wisselgeld. " x 20c munten".PHP_EOL;
+    $totaal -= $wisselgeld * 0.2;        
+    }
+
+    $wisselgeld = floor($totaal/0.1);
+    if ($wisselgeld != 0){
+    echo $wisselgeld. " x 10c munten".PHP_EOL;
+    $totaal -= $wisselgeld * 0.1;        
+    }
+
+    $wisselgeld = floor(round($totaal * 100 / 5));
+    if ($wisselgeld != 0){
+    echo $wisselgeld. " x 5c munten".PHP_EOL;
+    $totaal -= $wisselgeld * 0.05;        
+    }
+}
+
+else{
+    echo"er is geen wisselgeld";
+}
+
+?>
